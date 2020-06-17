8a39e8d1cc7f2ae9c08866a26a00e5c195f56096
v.bazhenov <sleem81@list.ru>
2020-06-17 20:35:13 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::view('/', 'welcome');
+
+
+
+Route::get('user/{id}/comment/{user_id}', function ($id,$user_id) {
+   // echo $id + $id2;
+   // echo $id2;
+});
+
+Route::get('user{id?}{user_id?}', function ($id=null,$user_id=null) {
+     echo $id + $user_id;
+     echo $user_id;
+});
+
+Route::get('task', function () {
+    $select = App\Task::all(); // вся таблица
+    return view('task.index', compact('select'));
+});
+
+Route::get('task/{id}', function ($id) {
+//    $select = App\Task::all();
+    $id = \Illuminate\Support\Facades\DB::table('tasks')->find($id); // строка
+    return view('task.task', compact('id'));
 });
